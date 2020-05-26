0ba9623226c0891621d4344c178baea8a7f1b6ba
BoziDaniel <daniel.bozi88@gmail.com>
2020-05-26 15:27:45 +0200

    created Todocontroller class

--- src/main/java/com/codecool/todoapp/controller/TodoController.java
+++ src/main/java/com/codecool/todoapp/controller/TodoController.java
@@ -1,4 +1,13 @@
 package com.codecool.todoapp.controller;
 
 public class TodoController {
+    // add new todo
+    //list todos by id
+    //remove all completed
+    //toggle all status
+    // Remove by id
+    // Update by id
+    // Find by id
+    // Toggle status by id
+
 }
