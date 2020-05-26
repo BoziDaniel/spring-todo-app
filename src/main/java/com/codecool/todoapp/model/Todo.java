7e9e9189cf913421dde57e7fc752e70b3bcb7a8c
BoziDaniel <daniel.bozi88@gmail.com>
2020-05-26 15:34:11 +0200

    added annotations instead of manual getters, setters and constructors

--- src/main/java/com/codecool/todoapp/model/Todo.java
+++ src/main/java/com/codecool/todoapp/model/Todo.java
@@ -1,5 +1,17 @@
 package com.codecool.todoapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Todo {
 
     private String title;
@@ -7,39 +19,9 @@ public class Todo {
     private Status status;
     private static int _idCounter = 0;
 
-    private Todo(String title, String id, Status status) {
-        this.title = title;
-        this.id = id;
-        this.status = status;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public boolean isCompleted() {
-        return this.status == Status.COMPLETE;
-    }
+//    public boolean isCompleted() {
+//        return this.status == Status.COMPLETE;
+//    }
 
     public static Todo create(String title) {
         _idCounter++;
