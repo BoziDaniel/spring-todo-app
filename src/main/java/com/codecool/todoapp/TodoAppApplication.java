0a1fa0370b73f2b87d26d9517b7579e46fcc7b31
BoziDaniel <daniel.bozi88@gmail.com>
2020-05-27 02:34:02 +0200

    added some init data to dao

--- src/main/java/com/codecool/todoapp/TodoAppApplication.java
+++ src/main/java/com/codecool/todoapp/TodoAppApplication.java
@@ -1,7 +1,16 @@
 package com.codecool.todoapp;
 
+import com.codecool.todoapp.model.Status;
+import com.codecool.todoapp.model.Todo;
+import com.codecool.todoapp.model.TodoDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class TodoAppApplication {
@@ -10,4 +19,19 @@ public class TodoAppApplication {
         SpringApplication.run(TodoAppApplication.class, args);
     }
 
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(TodoAppApplication.class);
+    @Autowired
+    private TodoDao todoDao;
+
+    @Bean
+    @Profile("dev")
+    public CommandLineRunner init() {
+        return args -> {
+            todoDao.add(new Todo("first item", Status.ACTIVE));
+            todoDao.add(new Todo("2 item", Status.ACTIVE));
+            todoDao.add(new Todo("3 item", Status.ACTIVE));
+            System.out.println(todoDao.toString());
+        };
+
+    }
 }
